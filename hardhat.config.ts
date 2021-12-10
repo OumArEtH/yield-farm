24398cf4f36f55ea23440cd41faab9c9675c9034
Moussa Oumarou <oumar@Moussas-MacBook-Pro-2.local>
2021-12-10 00:18:27 +0000

    adding contract verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,12 @@
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
 
 import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
 
 dotenv.config();
 
-const { DEPLOYER_PRIVATE_KEY, INFURA_API_KEY } = process.env;
+const { DEPLOYER_PRIVATE_KEY, INFURA_API_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
@@ -22,6 +23,9 @@ const config: HardhatUserConfig = {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`
   }
 };
 
