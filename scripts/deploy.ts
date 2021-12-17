ce390782f21e09058bc9ee0c26f12ff4566150fc
Moussa Oumarou <oumar@Moussas-MacBook-Pro-2.local>
2021-12-17 13:32:55 +0000

    some housekeeping

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,22 @@
-import { ethers } from 'hardhat';
+import { ethers } from 'hardhat'
 
 async function main() {
   
-  const PeriPeriGov = await ethers.getContractFactory("PeriPeriGov");
-  const ppgov = await PeriPeriGov.deploy(ethers.utils.parseEther("1000"));
-  await ppgov.deployed();
+  const PeriPeriGov = await ethers.getContractFactory("PeriPeriGov")
+  const ppgov = await PeriPeriGov.deploy(ethers.utils.parseEther("1000"))
+  await ppgov.deployed()
 
-  const network = await ethers.provider.getNetwork();
+  const network = await ethers.provider.getNetwork()
 
-  console.log("PeriPeriGov deployed at: %s on network %s", ppgov.address, network.name);
+  console.log("PeriPeriGov deployed at: %s on network %s", ppgov.address, network.name)
 
-  const YieldFarmingManager = await ethers.getContractFactory("YieldFarmingManager");
-  const yFManager = await YieldFarmingManager.deploy(ppgov.address, 0, 10, 1);
-  await yFManager.deployed();
+  const YieldFarmingManager = await ethers.getContractFactory("YieldFarmingManager")
+  const yFManager = await YieldFarmingManager.deploy(ppgov.address, 0, 10, 1)
+  await yFManager.deployed()
 
-  console.log("YieldFarmingManager deployed at: %s on network %s", yFManager.address, network.name);
+  console.log("YieldFarmingManager deployed at: %s on network %s", yFManager.address, network.name)
 
-  const tx = await ppgov.transfer(yFManager.address, ethers.utils.parseEther("100"));
+  const tx = await ppgov.transfer(yFManager.address, ethers.utils.parseEther("100"))
 
   console.log("Sent initial reward tokens to YieldFarmingManager. More details %s", tx.hash)
 }
@@ -24,6 +24,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
+    console.error(error)
+    process.exit(1)
   });
